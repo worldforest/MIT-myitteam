2a8d592c4bc502a026aee4d744d6ee1f7c34cac1
김지훈 <jh8039@naver.com>
2020-07-22 17:33:33 +0900

    실험

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <v-app>
-    <router-link class="black--text" to="/signup">SIGNUP</router-link>
-    <v-main>
-      <router-view/>
-    </v-main>
+    <v-container>
+      <router-link class="black--text" to="/signup">SIGNUP</router-link>
+      <router-link to="/profile">Profile</router-link>
+      <router-link to="/">Home</router-link>
+      <v-main>
+        <router-view/>
+      </v-main>
+    </v-container>
   </v-app>
 </template>
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,13 +5,11 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld
+
   }
 }
 </script>
