7a3c2b33c74378370f8535b988fc104aff8cc90a
hong jeong min <wjdals0471@naver.com>
2020-07-22 15:59:09 +0900

    user controller User 추가

--- backend/SNSspring/src/main/java/com/mit/controller/UserController.java
+++ backend/SNSspring/src/main/java/com/mit/controller/UserController.java
@@ -1,5 +1,47 @@
 package com.mit.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mit.dto.User;
+
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+
+	// Service
+	@ApiOperation(value = "로그인 처리")
+	@PostMapping("login")
+	public ResponseEntity<String> login(@RequestBody User user) {
+		user = userService.login(user);
+		if (user.getUseremail() != null) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<User>(userService.login(user), HttpStatus.OK);
+	}
 
 }
