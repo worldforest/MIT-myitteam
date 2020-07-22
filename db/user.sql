d661a2cae76003e779cd234621910f94e5ec1fdf
hong jeong min <wjdals0471@naver.com>
2020-07-23 03:04:40 +0900

    admin 계정 오타 수정

--- db/user.sql
+++ db/user.sql
@@ -1,7 +1,7 @@
 use mydb;
 select * from user;
 insert into user (email, pwd, name, nickname, age, gender, major, city, gu, dong, details)
-values("dmin@naver.com", 'admin', '관리자','admin', '99', '여자', '컴퓨터공학', '대전시', '유성구', '봉명동', '봉명카페');
+values("admin@naver.com", 'admin', '관리자','admin', '99', '여자', '컴퓨터공학', '대전시', '유성구', '봉명동', '봉명카페');
 insert into user (email, pwd, name, nickname, age, gender, major, city, gu, dong, details)
 values("serim@naver.com", 'qwer1234', '이세림','rim', '24', '여자', '정보통신', '대전시', '유성구', '관평동', '배울1로');
 insert into user (email, pwd, name, nickname, age, gender, major, city, gu, dong, details)
