2664b22ee0350080253313c3af0c2fe56391bd8a
MIT(SNS PJT) <worldforest99@gmail.com>
2020-07-23 16:55:20 +0900

    edit swagger

--- backend/SNSspring/src/main/java/com/mit/config/SwaggerConfig.java
+++ backend/SNSspring/src/main/java/com/mit/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //swagger 사용법
-//http://localhost:9999/swagger-ui.html#/
+//http://localhost:9999/mit/swagger-ui.html
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
