4387ab5b36899dba2269c3dd976b93808a1ecac9
MIT(SNS PJT) <worldforest99@gmail.com>
2020-07-27 18:12:08 +0900

    backend project

---
+++ backend/SNSspring/src/main/java/com/mit/service/ContentsService.java
@@ -0,0 +1,7 @@
+package com.mit.service;
+
+import com.mit.dto.Contents;
+
+public interface ContentsService {
+	public boolean insert(Contents contents);
+}
---
+++ backend/SNSspring/src/main/java/com/mit/service/ContentsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mit.dto.Contents;
+import com.mit.repo.ContentsRepo;
+
+@Service
+public class ContentsServiceImpl implements ContentsService {
+
+	@Autowired
+	private ContentsRepo contentsRepo;
+	
+	@Override
+	public boolean insert(Contents contents) {
+		return contentsRepo.insert(contents);
+	}
+
+}
