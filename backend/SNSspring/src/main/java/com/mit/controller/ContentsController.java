195f182dc072c59e7edd3cc8f079fe9e00a103ed
MIT(SNS PJT) <worldforest99@gmail.com>
2020-07-29 17:37:53 +0900

    S03P12B306-142 DB에서 공모전 정보 GET

--- backend/SNSspring/src/main/java/com/mit/controller/ContentsController.java
+++ backend/SNSspring/src/main/java/com/mit/controller/ContentsController.java
@@ -1,5 +1,7 @@
 package com.mit.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +23,7 @@ import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
-@RequestMapping("/api/contests")
+@RequestMapping("/api/contents")
 public class ContentsController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	private static final String SUCCESS = "success";
@@ -29,7 +32,7 @@ public class ContentsController {
 	@Autowired
 	private ContentsService contentsService;
 
-	@ApiOperation(value = "공모전, 프로젝트 등록 ", notes = "성공시 200, 실패시 에러를 반환합니다. \n ")
+	@ApiOperation(value = "공모전 등록 ", notes = "성공시 200, 실패시 에러를 반환합니다. \n ")
 	@PostMapping("create/contest")
 	public ResponseEntity<String> createContests(@RequestBody Contents contents, HttpServletRequest request) {
 //		int category=contents.getCategory();
@@ -42,12 +45,19 @@ public class ContentsController {
 		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 	}
 	
-	@ApiOperation(value = "공모전 등록 ", notes = "성공시 200, 실패시 에러를 반환합니다. \n ")
+	@ApiOperation(value = "프로젝트 등록 ", notes = "성공시 200, 실패시 에러를 반환합니다. \n ")
 	@PostMapping("create/project")
 	public ResponseEntity<String> createProject(@RequestBody Contents contents, HttpServletRequest request) {
 		contents.setCategory(1);
 		contentsService.insert(contents);
 		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "공모전 조회 ", notes = "성공시 200, 실패시 에러를 반환합니다. \n ")
+	@GetMapping("readAll/contest")
+	public List<Contents> readContests() {
+		List<Contents> contestAll = contentsService.selectAll();
+		return contestAll;
 	}
 
 }
--- backend/SNSspring/src/main/java/com/mit/repo/ContentsRepo.java
+++ backend/SNSspring/src/main/java/com/mit/repo/ContentsRepo.java
@@ -1,10 +1,13 @@
 package com.mit.repo;
 
+import java.util.List;
+
 import com.mit.dto.Contents;
 
 public interface ContentsRepo {
 	public boolean insert(Contents contents);
-	public boolean read(Contents contents);
+	public List<Contents> selectAll();
+	public Contents selectOne(String no);
 	public boolean update(Contents contents);
 	public boolean delete(Contents contents);
 }
