ad8baa3c66380688b2082355a4258ebca8dee127
hong jeong min <wjdals0471@naver.com>
2020-07-30 10:01:37 +0900

    return test 추가

--- backend/SNSspring/src/main/java/com/mit/controller/TeamController.java
+++ backend/SNSspring/src/main/java/com/mit/controller/TeamController.java
@@ -1,6 +1,13 @@
 package com.mit.controller;
-//팀원 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//팀원 
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/api/team")
 public class TeamController {
 	
 }
