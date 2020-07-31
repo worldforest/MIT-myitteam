777f7b2416313f381928fe5773996da68cb7c09d
MIT(SNS PJT) <worldforest99@gmail.com>
2020-07-31 21:12:26 +0900

    S03P12B306-143 follow/unfollow

--- backend/SNSspring/src/main/java/com/mit/repo/FollowRepo.java
+++ backend/SNSspring/src/main/java/com/mit/repo/FollowRepo.java
@@ -1,6 +1,13 @@
 package com.mit.repo;
 
+import com.mit.dto.Follow;
+
 public interface FollowRepo {
+	public boolean follow(Follow follow);
+
+	public boolean unfollow(Follow follow);
+
 	public int followingCnt(String follower);
+
 	public int followerCnt(String following);
 }
--- backend/SNSspring/src/main/java/com/mit/service/FollowService.java
+++ backend/SNSspring/src/main/java/com/mit/service/FollowService.java
@@ -1,6 +1,10 @@
 package com.mit.service;
 
+import com.mit.dto.Follow;
+
 public interface FollowService {
+	public boolean follow(Follow follow);
+	public boolean unfollow(Follow follow);
 	public int followingCnt(String follower);
 	public int followerCnt(String following);
 }
