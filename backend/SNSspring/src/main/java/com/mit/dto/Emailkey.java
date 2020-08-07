786cc74c5a7a83bcb1810f109742768c4640850a
redbin <wjdals0471@naver.com>
2020-08-07 15:25:22 +0900

    이메일 인증기능 보안성 문제추가

---
+++ backend/SNSspring/src/main/java/com/mit/dto/Emailkey.java
@@ -0,0 +1,19 @@
+package com.mit.dto;
+
+public class Emailkey {
+	String email;
+	String emailkey;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmailkey() {
+		return emailkey;
+	}
+	public void setEmailkey(String emailkey) {
+		this.emailkey = emailkey;
+	}
+	
+}
