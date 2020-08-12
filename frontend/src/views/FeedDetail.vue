06afbe1eca4ffc5ee8fbfebe4cb4748bb11619cf
unknown <dlekdls0213@naver.com>
2020-08-12 22:41:34 +0900

    list

--- frontend/src/views/FeedDetail.vue
+++ frontend/src/views/FeedDetail.vue
@@ -59,7 +59,9 @@
         <v-icon v-if="likeUserList2.includes(email)" large class="mr-4 likebtn" color="red" @click="unlike(likeData);">mdi-heart-multiple</v-icon>
       </v-row>
     </div>
-    <div v-else>
+
+    <!-- 크기 작을 때 !!!!!!!!!!!!!!!1 -->
+    <div v-if="windowWidth < 767">
       <router-link :to="{name: 'UserProfile', params:{user:detailFeed.email}}" v-if="email !== detailFeed.email"><h3>작성자: {{ detailFeed.nickname }}</h3></router-link>
       <router-link to='/profile' v-else><h3>작성자: {{ detailFeed.nickname }}</h3></router-link>
       <hr>
@@ -77,6 +79,45 @@
       <div>
         {{ detailFeed.tag }}
       </div>
+
+      <hr>
+      <!-- 좋아요를 시작해보지 -->
+      {{ likeUserList }}
+      <v-row>
+        <v-spacer></v-spacer>
+
+        <div class="text-center">
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <span v-bind="attrs" v-on="on" class="cursor">
+                <h5 class="mr-4">{{ likeCntnum }}명이 좋아합니다. </h5>
+              </span>
+            </template>
+
+            <!-- {{ i.datalist }} -->
+            <v-card class="cardModal">
+              <!-- {{ followList }} -->
+              <h3 class="modaltitle mb-3"> 좋아요 누른 사람</h3>
+              
+              <li v-for="item in likeUserList" :key="item">
+                <router-link :to="{name: 'UserProfile', params:{user:item.email}}" class="followa"><h4 class="ml-3">{{ item.nickname }}</h4></router-link>
+                <hr>
+              </li>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="local" color="primary" text @click="dialog = false">
+                  <h5>닫기</h5>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+
+        <v-icon v-if="!likeUserList2.includes(email)" large class="mr-4 likebtn" @click="like(likeData);">mdi-heart-multiple-outline</v-icon>
+        <!-- <h5 v-if="likeUserList.includes(email)" class="mr-4">본인을 포함한 외 {{ likeCntnum }}명이 좋아합니다. </h5> -->
+        <v-icon v-if="likeUserList2.includes(email)" large class="mr-4 likebtn" color="red" @click="unlike(likeData);">mdi-heart-multiple</v-icon>
+      </v-row>
     </div>
   </div>
 </template>
