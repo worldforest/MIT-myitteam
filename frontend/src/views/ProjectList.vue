dcf60ceec622f0a2f2f0dd1f12524168c8059a6b
MIT(SNS PJT) <worldforest99@gmail.com>
2020-08-19 16:14:14 +0900

    프로젝트 카드 글씨 수정 및 자세히 보기 버튼 삭제

--- frontend/src/views/ProjectList.vue
+++ frontend/src/views/ProjectList.vue
@@ -20,7 +20,7 @@
               <h4 class="mx-auto mt-3" v-else>{{ i.title }}</h4>
             </v-card-title>
             <hr class="hrr">
-            <v-card-text class="text--primary mb-3">{{ i.start }} ~ {{ i.end }}
+            <v-card-text class="text--primary mb-3"><h5>{{ i.start }} ~ {{ i.end }}</h5>
             </v-card-text>
           </div>
         </v-col>
@@ -81,10 +81,15 @@ export default {
   .hrr{
     width: 200px;
     margin: 0 auto;
+    height: 5px;
   }
   h4 {
+    font-family: 'Do Hyeon', sans-serif;
     color: rgb(92, 107, 192);
-    font-weight: bold;
+  }
+  h5 {
+    font-family: 'Do Hyeon', sans-serif;
+    color: black;
   }
   li {
     list-style: none;
